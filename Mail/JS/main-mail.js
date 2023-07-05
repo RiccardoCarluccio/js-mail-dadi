ac0907965964e12615d8e19f707bdcf7ff52ef73
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-05 16:41:41 +0200

    email generator v0.1 - created pref e suff vars

--- Mail/JS/main-mail.js
+++ Mail/JS/main-mail.js
@@ -2,6 +2,13 @@ const btnEmailCheck = document.querySelector(".btn-email-check");
 
 const userEmail = document.getElementById("user-email");
 
+const emailPrefixesStringed = 'abcdefghijklmnopqrstuvwxyz';
+const emailPrefixes = emailPrefixesStringed.split("");
+const emailSuffix = '@boolean.com';
+
+
+
+
 btnEmailCheck.addEventListener('click', function getData() {
   const emailInput = userEmail.value;
 
