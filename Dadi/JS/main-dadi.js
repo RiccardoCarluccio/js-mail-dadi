e3524b6940a9a66c4989a5af99481316c612a7c2
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-05 18:06:26 +0200

    Dadi project ended and working (no bonus yet)

--- Dadi/JS/main-dadi.js
+++ Dadi/JS/main-dadi.js
@@ -4,6 +4,17 @@ btnRoll.addEventListener('click', function roll() {
   const userResult = Math.floor(Math.random() * 6) + 1;
   const cpuResult = Math.floor(Math.random() * 6) + 1;
 
-  console.log(userResult);
-  console.log(cpuResult);
+  console.log(userResult);  //per testare
+  console.log(cpuResult);  //in console
+
+  document.getElementById("user-roll").innerHTML = `You rolled: ${userResult}`;
+  document.getElementById("cpu-roll").innerHTML = `The computer rolled: ${cpuResult}`;
+
+  if (userResult > cpuResult) {
+    document.getElementById("result-message").innerHTML = 'You won the roll'
+  } else if (userResult === cpuResult) {
+    document.getElementById("result-message").innerHTML = 'Draw'
+  } else {
+    document.getElementById("result-message").innerHTML = 'You lost the roll'
+  }
 })
