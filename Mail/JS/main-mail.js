3624ff9fb07aa3931c755fbc83922d690b8b5a68
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-05 16:53:14 +0200

    email generator completed and working

--- Mail/JS/main-mail.js
+++ Mail/JS/main-mail.js
@@ -2,12 +2,16 @@ const btnEmailCheck = document.querySelector(".btn-email-check");
 
 const userEmail = document.getElementById("user-email");
 
-const emailPrefixesStringed = 'abcdefghijklmnopqrstuvwxyz';
-const emailPrefixes = emailPrefixesStringed.split("");
-const emailSuffix = '@boolean.com';
+const emailPrefixesStringed = 'abcdefghijklmnopqrstuvwxyz'; //mi conviene
+const emailPrefixes = emailPrefixesStringed.split("");      //metterle
+const emailSuffix = '@boolean.com';                         //dentro il for?
+const emailArray = [];
 
+for (i = 0; i < emailPrefixes.length; i++) {
+  emailArray[i] = emailPrefixes[i] + emailSuffix;
+}
 
-
+console.log(emailArray);
 
 btnEmailCheck.addEventListener('click', function getData() {
   const emailInput = userEmail.value;
